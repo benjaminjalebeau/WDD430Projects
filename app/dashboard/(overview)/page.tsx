b50f01a58f59f75dde0ca19846dc53b8326cfd85
benjaminjalebeau <benjamin.ja.lebeau@gmail.com>
2025-03-19 22:17:35 -0600

    put lint back in and actually fixed the problems.

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,7 +2,6 @@ import CardWrapper from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react';
 import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
 import { Metadata } from 'next';
@@ -14,12 +13,6 @@ export const metadata: Metadata = {
  
 export default async function Page() {
 
-  const {
-    numberOfInvoices,
-    numberOfCustomers,
-    totalPaidInvoices,
-    totalPendingInvoices,
-  } = await fetchCardData();
   return (
     
     <main>
